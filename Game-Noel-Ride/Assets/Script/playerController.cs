f2466278678a19b5493ecce47f61b4ad7bdbe079
Remyson Costa <remysonrodrigues@gmail.com>
2020-07-01 21:42:56 -0300

    add animações

---
+++ Game-Noel-Ride/Assets/Script/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float velocidadeMovimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        playerRb.velocity = new Vector2(horizontal * velocidadeMovimento, vertical * velocidadeMovimento);
+    }
+}
