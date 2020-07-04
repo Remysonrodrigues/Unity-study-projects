58d3400830e89e331c6f3cc769a985ee47177b01
Remyson Costa <remysonrodrigues@gmail.com>
2020-07-04 10:09:42 -0300

    add collid2D

--- Game-Noel-Ride/Assets/Script/playerController.cs
+++ Game-Noel-Ride/Assets/Script/playerController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
     private Rigidbody2D playerRb;
     public float velocidadeMovimento;
+    
+    private int presentes;
+    public int tempoFase;
+
+    public Text presenteTxt;
+    public Text tempoTxt;
+
+    public GameObject particula;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        StartCoroutine("contagemRegressiva");
     }
 
     // Update is called once per frame
@@ -19,5 +30,35 @@ public class playerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         playerRb.velocity = new Vector2(horizontal * velocidadeMovimento, vertical * velocidadeMovimento);
+    }
+
+    IEnumerator contagemRegressiva()
+    {
+        tempoTxt.text = tempoFase.ToString();
+        yield return new WaitForSeconds(1);
+
+        tempoFase -= 1;
+
+        if (tempoFase > 0)
+        {
+            StartCoroutine("contagemRegressiva");
+        }
+        else
+        {
+            SceneManager.LoadScene("Titulo");
+        }
+    }
+
+    void OnTriggerEnter2D( Collider2D col )
+    {
+        if (col.tag == "presente")
+        {
+            presentes += 1;
+            presenteTxt.text = presentes.ToString();
+
+            Instantiate(particula, col.transform.position, col.transform.rotation);
+
+            Destroy(col.gameObject);
+        }
     }
 }
